8b689c207d5819f2b6b2f4d2d08cbcabf055dd8b
laibin5 <laibin5@gdii-yueyun.com>
2021-10-21 11:33:08 +0800

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Appmb from './Appmb';
 import "antd/dist/antd.css";
 import 'normalize.css/normalize.css';
 import 'utils/i18n';
 
-ReactDOM.render(<Appmb/>, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
